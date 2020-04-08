23b1f8d1618b3f7072415975239052bee3cc86f1
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2020-04-08 09:58:42 +0700

    Commit1

---
+++ src/Program1.py
@@ -0,0 +1,9 @@
+listku = [10, 20, 30]
+
+a = listku[0]
+b = listku[1]
+c = listku[2]
+
+print(f'List = {listku}')
+print(f'a = {a}, b = {b}, c = {c}')
+
---
+++ src/Program2.py
@@ -0,0 +1,6 @@
+listku = [10, 20, 30]
+a, b, c = listku
+
+print(f'List = {listku}')
+print(f'a = {a}, b = {b}, c = {c}')
+
